70fd867540765da408bd202f0b0979ad2e3a928f
Syed Ahmed Hassan <syedahmedh495@gmail.com>
2025-02-26 17:14:20 +0500

    growth.py

--- growth.py
+++ growth.py
@@ -78,7 +78,7 @@ if uploaded_files:
         if st.button(f"Conver {file.name}"):
             buffer = BytesIO()
             if conversion_type == "CSV":
-                df.to.csv(buffer, index=False)
+                df.to_csv(buffer, index=False)
                 file_name = file.name.replace(file_ext, ".csv")
                 mime_type = "text/csv"
             
